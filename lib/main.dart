c97a3e47e41134fa0902f515d4c7bd054059ab5a
Kiran Saleem <saleemkiran1599@gmail.com>
2024-12-18 21:17:52 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: SafeArea(
-              child: Center(
-                  child: Container(
-        child: Text(
-          'Hello World',
-          style: TextStyle(fontSize: 18, color: Colors.white),
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              child: Text(
+                'Hello World',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              color: Colors.deepOrange,
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+              margin: EdgeInsets.only(left: 10, top: 20),
+            ),
+          ),
         ),
-        color: Colors.deepOrange,
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
-        margin: EdgeInsets.only(left: 10, top: 20),
-      )))),
+      ),
     );
   }
 }
